029054ebcb9dd151e43d241d4e087a26f9f8f65b
Ayush Gupta <ag8350961@gmail.com>
2023-08-15 23:25:28 +0530

    msg

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,12 +4,30 @@ import {Photo1,Photo2} from "./Photo";
 
 function Project(){
     return(
-        <div className="container" style={{flexWrap:"wrap" ,  marginTop: "200px" ,justifyContent:"space-between"}} id="3">
+        <div className="container" style={{flexWrap:"wrap" ,  marginTop: "200px" }} id="3">
             <div className="heading">My Projects</div>
-            <Photo1 />
-            <Item1 />
-            <Photo2 />
-            <Item2 />
+            <div
+                style={{
+                display: "flex",
+                flexWrap: "wrap",
+                marginTop: "20px",
+                justifyContent: "center"
+                }}
+            >
+                <Photo1 />
+                <Item1 />
+            </div>
+            <div
+                style={{
+                display: "flex",
+                flexWrap: "wrap-reverse",
+                marginTop: "50px",
+                justifyContent: "center"
+                }}
+            >
+                <Item2 />
+                <Photo2 />
+            </div>
         </div>
     );
 }
