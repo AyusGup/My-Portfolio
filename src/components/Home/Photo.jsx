e6f29f15e2f57f43412a84494fc07fae9ca36896
Ayush <ag8350961@gmail.com>
2026-03-10 15:58:17 +0530

    fixed lint

--- src/components/Home/Photo.jsx
+++ src/components/Home/Photo.jsx
@@ -1,7 +1,7 @@
 function Photo(props) {
   return(
     <div className="flex justify-center w-[100%] lg:w-[30%]">
-      <img src={props.logo} alt="Your Image" className="max-w-64 max-h-64 rounded-md" />
+      <img src={props.logo} alt="Logo" className="max-w-64 max-h-64 rounded-md" />
     </div>
   );
 }
